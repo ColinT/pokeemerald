0836d120abaa542c08660dfb6a9c4a24407271c4
AJ Fontaine <36677462+Fontbane@users.noreply.github.com>
2019-05-17 21:07:44 -0400

    Fix some typos in item names

--- src/data/items.h
+++ src/data/items.h
@@ -4991,7 +4991,7 @@ const struct Item gItems[] =
 
     [ITEM_POKE_FLUTE] =
     {
-        .name = _("Poké FLUTE"),
+        .name = _("Poké Flute"),
         .itemId = ITEM_POKE_FLUTE,
         .price = 0,
         .holdEffect = HOLD_EFFECT_NONE,
@@ -5197,7 +5197,7 @@ const struct Item gItems[] =
 
     [ITEM_COVER_FOSSIL] =
     {
-        .name = _("Covet Fossil"),
+        .name = _("Cover Fossil"),
         .itemId = ITEM_COVER_FOSSIL,
         .price = 0,
         .description = sCoverFossilDesc,
