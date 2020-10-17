75ae07d8647529e89d74cc1ac2fe9b0f2c2c24c2
Eduardo Quezada <eduardo602002@gmail.com>
2020-10-17 02:33:15 -0300

    Dive Ball config.

--- src/battle_main.c
+++ src/battle_main.c
@@ -5061,6 +5061,7 @@ static void FreeResetData_ReturnToOvOrDoEvolutions(void)
     if (!gPaletteFade.active)
     {
         gIsFishingEncounter = FALSE;
+        gIsSurfingEncounter = FALSE;
         ResetSpriteData();
         if (gLeveledUpInBattle == 0 || gBattleOutcome != B_OUTCOME_WON)
         {
